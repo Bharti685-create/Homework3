bf14012c0af94713d45758e00cde210d412a841d
Bharti Shinde <Bharti.Shinde01@student.csulb.edu>
2020-09-25 21:12:24 -0700

    Correction

--- Solution311.py
+++ Solution311.py
@@ -1,19 +1,21 @@
 """Calculating miles per gallon"""
-
 # Initializing the variable for the values
 
 SENTINEL = -1 #SENTINEL is a special value that marks the end
 miles = 0 
 gallons = 0
-total_miles = 0
-total_gallons_used = 0
+avg_miles_per_gallon = 0
 
 while True: # loop until break
-    miles = int(input('Enter the number of miles driven:')) # prompting user to enter the input
-    if miles == SENTINEL:
+    current_g_reading = float(input('Enter gallons used : ')) # prompting user to enter the input
+    if current_g_reading == SENTINEL:
         break # if SENTINEL is entered break the loop
-    gallons_used = float(input('Enter the number of gallons of gas used(enter -1 to exit):')) #prompting user to enter the input
-    print(f'The number of gallons of gas used is {gallons_used}') # print gallons_used
-    print(f'The miles driven {miles}') #print miles driven
-    miles_per_gallon = miles / gallons_used #calculate miles per gallon
-    print(f'The miles_per_gallon is {miles_per_gallon: .5f}') #print miles_per_gallon with five digits after decimal
+
+    gallons_used = current_g_reading + gallons
+    current_m_reading = float(input('Enter miles driven: '))
+    miles = miles + current_m_reading
+    current_avg_reading = current_m_reading / current_g_reading 
+    print('Avg miles / gallon : ' + str(current_avg_reading))
+
+avg_miles_per_gallon = miles / gallons
+print('Avg miles / gallon : ' + str(avg_miles_per_gallon))
