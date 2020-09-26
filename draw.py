07345f504aa3594ca51adb27252cb44d763906db
Bharti Shinde <Bharti.Shinde01@student.csulb.edu>
2020-09-25 21:39:12 -0700

    CorrectionT

--- draw.py
+++ draw.py
@@ -9,10 +9,11 @@ pen.forward(100) #moves the turtle forward with the specified distance 100
 pen.setheading(225)#sets the orientation of turtle to an angle 225
 pen.forward(70) #moves the turtle forward with the specified distance of 70
 pen.penup() # pulls up the pen, no drawing while the pen moves
+pen.hideturtle() #hides turtle
 pen.goto(0, 0) # moves the pen to an absolute direction, without drawing a line
 pen.pendown() # pulls the pen down, will draw lines while moving
-pen.color('Red') # sets the pen color to red
 pen.showturtle() # makes the turtle visible
+pen.color('Red') # sets the pen color to red
 pen.setheading(135) #sets the orientation of turtle to an angle 315
 pen.forward(70) #moves the turtle forward with the specified distance of 70
 pen.setheading(225) #sets the orientation of turtle to an angle 225
